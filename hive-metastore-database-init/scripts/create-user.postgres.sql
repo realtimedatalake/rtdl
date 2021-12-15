645bc9b53615445e1a78353302db6fc9ad0dd677
Gavin Johnson <gavinj1984@gmail.com>
2021-12-15 12:08:08 -0800

    got db init working

--- hive-metastore-database-init/scripts/create-user.postgres.sql
+++ hive-metastore-database-init/scripts/create-user.postgres.sql
@@ -1,2 +1,13 @@
-CREATE ROLE metastore LOGIN PASSWORD 'metastore';
-CREATE DATABASE metastore_db OWNER metastore;
+SELECT 'CREATE DATABASE metastore_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'metastore_db')\gexec
+
+DO
+$do$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE  rolname = 'metastore') THEN
+        CREATE ROLE metastore LOGIN PASSWORD 'metastore';
+    END IF;
+END
+$do$;
+
+ALTER DATABASE metastore_db OWNER TO metastore;
