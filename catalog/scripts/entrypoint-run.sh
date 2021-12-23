27a34bc7a64bcf5e2640557dab01f2c74fa1582b
dipanjanb <dipanjanb@gmail.com>
2021-12-23 13:47:30 +0530

    Modifications for a) running on M1 b) resolving multiple SLF4J binding issue

--- catalog/scripts/entrypoint-run.sh
+++ catalog/scripts/entrypoint-run.sh
@@ -6,5 +6,5 @@ export METASTORE_VERSION=3.1.2
 export JAVA_HOME=/usr/local/openjdk-8
 export HADOOP_HOME=/opt/hadoop-${HADOOP_VERSION}
 export HIVE_HOME=/opt/apache-hive-metastore-${METASTORE_VERSION}-bin
-
+export CLASSPATH=`echo $CLASSPATH| sed 's/\/slf4j\-log4j12\-1\.7\.30\.jar//g'`
 ${HIVE_HOME}/bin/start-metastore
