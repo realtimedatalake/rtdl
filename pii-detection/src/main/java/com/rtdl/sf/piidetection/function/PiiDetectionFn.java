5024e8ea1977f90224547f86b390e5f38cb3af19
Emre Baykal <baykalemre@gmail.com>
2022-03-09 14:33:46 +0300

    updated redpanda version in docker-compose.yml and docker-compose.init.yml added log4j dependency in pom.xml added logging in PiiDetectionFn
    Signed-off-by: Emre Baykal <baykalemre@gmail.com>

--- pii-detection/src/main/java/com/rtdl/sf/piidetection/function/PiiDetectionFn.java
+++ pii-detection/src/main/java/com/rtdl/sf/piidetection/function/PiiDetectionFn.java
@@ -6,6 +6,8 @@ import org.apache.flink.statefun.sdk.java.StatefulFunction;
 import org.apache.flink.statefun.sdk.java.TypeName;
 import org.apache.flink.statefun.sdk.java.io.KafkaEgressMessage;
 import org.apache.flink.statefun.sdk.java.message.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,19 +15,22 @@ public class PiiDetectionFn implements StatefulFunction {
 
     public static final TypeName PII_TYPE = TypeName.typeNameFromString("com.rtdl.sf/pii-detection");
     public static final TypeName PII_EGRESS = TypeName.typeNameFromString("com.rtdl.sf/pii-egress");
+    private static final Logger LOG = LoggerFactory.getLogger(PiiDetectionFn.class);
 
     @Override
     public CompletableFuture<Void> apply(Context context, Message message) {
         if (!message.is(IncomingMessage.TYPE)) {
+            LOG.error("Unknown type");
             throw new IllegalStateException("Unknown type");
         }
 
         IncomingMessage incomingMessage = message.as(IncomingMessage.TYPE);
+        LOG.info("Incoming:" + incomingMessage.toString());
 
         context.send(
                 KafkaEgressMessage.forEgress(PII_EGRESS)
                         .withTopic("pii-detection")
-                        .withUtf8Value("Testing pii detection... Incoming message:  " + incomingMessage.toString())
+                        .withUtf8Value("Testing pii detection... Incoming message:  " + incomingMessage)
                         .build());
 
         return context.done();
